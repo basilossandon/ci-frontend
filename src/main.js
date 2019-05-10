c9ebdb8d8e029db713e27994dd70172a5d983ca9
l-pizarro <leandro.pizarro@usach.cl>
2019-05-09 22:31:31 -0400

    Test

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@ import locale from "element-ui/lib/locale/lang/en";
 Vue.use(ElementUI, locale);
 Vue.config.productionTip = false;
 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 new Vue({
   render: function(h) {
     return h(App);
