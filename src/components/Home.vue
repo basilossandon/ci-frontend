c897d6b2588156ecafdc5da8cfd342de16c571fa
l-pizarro <leandro.pizarro@usach.cl>
2019-05-09 21:19:43 -0400

    Backgorund on Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="Home"></div>
+</template>
+<script>
+import axios from "axios";
+export default {
+	name: "Home",
+	components: {
+
+	}
+};
+</script>
+<style>
+	.Home {
+		background: url(../assets/background.svg) no-repeat center center fixed; 
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		height: 100vh;
+		width: 100%;
+	}
+
+</style>
