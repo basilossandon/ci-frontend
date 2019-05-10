832b160b709880cf27542ae6a2b400e5a8ccbeb1
l-pizarro <leandro.pizarro@usach.cl>
2019-05-09 20:44:33 -0400

    App component error component fixed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-	<div id="app">
-		<Svg/>
-	</div>
+  <div id="app">
+    <Svg/>
+  </div>
 </template>
-<script>
-import axios from "axios";
-import Svg from './component/Svg.vue';
 
+<script>
+import Svg from "./components/Svg.vue";
 export default {
-	name: "app",
-	components: {
-		Svg
-	}
+  name: "app",
+  components: {
+    Svg
+  },
+  data() {
+    return {};
+  }
 };
 </script>
+
 <style>
-	#app {
-		font-family: "Avenir", Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en";
 
-Vue.use(ElementUI);
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI, locale);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
